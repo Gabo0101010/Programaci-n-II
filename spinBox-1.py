f1cf193537f89f85185b92c9fbee8d1ea2b75b0b
Gabo0101010 <96912007+Gabo0101010@users.noreply.github.com>
2025-09-05 11:41:31 -0400

    Update and rename frames.py to spinBox-1.py

---
+++ spinBox-1.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import messagebox, ttk
+ 
+ventanaPrincipal=tk.Tk()
+ 
+labelEdad=tk.Label(ventanaPrincipal, text="Edad:")
+labelEdad.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+spin=tk.Spinbox(ventanaPrincipal, from_=1, to=100)
+spin.grid(row=0, column=1, padx=10, pady=10)
+boton=tk.Button(ventanaPrincipal, text="Obtener valor", command=lambda:mostrarEdad())
+boton.grid(row=3, column=0, padx=10, pady=10)
+ 
+ventanaPrincipal.mainloop()
+
